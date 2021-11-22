37e401a5c46c5f5f3db5a14637e2eaf1e2d67957
Luke Frazer <71099197+Luke-Frazer@users.noreply.github.com>
2021-11-22 11:43:19 -0700

    Added files for the Zendesk Coding Challenge
    The two main files are ZCC.Class.java and ZCC_Main.java. The main is designed to run the entire process.

---
+++ Zendesk_Coding_Challenge/src/zcc_package/ZCC_Main.java
@@ -0,0 +1,24 @@
+package zcc_package;
+
+/**
+ * 
+ * @author Luke Frazer
+ * 
+ * Main class to call the coding challenge process
+ *
+ */
+public class ZCC_Main
+   {
+      /**
+       * @param args
+       */
+      public static void main(String[] args)
+         {
+            // create zendesk coding challenge class object
+            ZCC_Class ZCC = new ZCC_Class();
+            
+            // start the process
+            ZCC.getProcess();
+         }
+
+   }
